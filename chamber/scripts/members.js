6e73dbdc549913673d10cb6d7db4e6b4067adb70
florenciataravella <bunibliss@gmail.com>
2025-11-08 11:57:50 -0300

    h

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,3 +1,28 @@
+const navbutton=document.querySelector("#ham-btn");
+const navlinks=document.querySelector("#nav-bar");
+navbutton.addEventListener("click",()=>{
+    navbutton.classList.toggle("show");
+    navlinks.classList.toggle("show");
+})
+document.addEventListener("DOMContentLoaded",()=>{
+
+const yearjs=document.querySelector("#year");
+
+const year = new Date();
+
+yearjs.innerHTML=year.getFullYear();
+
+
+const lastmodifiedjs=document.querySelector("#lastmodified");
+const lastModified = new Date(document.lastModified);
+const formattedDate = new Intl.DateTimeFormat("en-US", {
+  dateStyle: "short",
+  timeStyle: "short"
+}).format(lastModified);
+
+lastmodifiedjs.textContent = formattedDate;
+});
+
 const url="https://florenciataravella.github.io/wdd231/chamber/members.json";
 const businessMembers=document.querySelector(".business-members");
 async  function getMembersInfo(){
